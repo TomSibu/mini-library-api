479def8d712e44bd6573d7476afc52ae61d62c2e
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2026-02-19 22:11:26 +0530

    Added JWT authentication and user registration API

---
+++ backend/books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    pages = models.IntegerField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ backend/users/urls.py
@@ -0,0 +1,12 @@
+# Login will automatically return:
+# -> Access Token
+# -> Refresh Token (JWT)
+
+from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+]
